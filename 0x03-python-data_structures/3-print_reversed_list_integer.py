560342f0e4b7a7f41ec57ea79a662985a31afa1a
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-08-15 03:55:52 -0700

    reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     n = len(my_list) - 1
     if n < 0:
-        print("")
+        print("list = None")
     while n >= 0:
         print("{:d}".format(my_list[n]))
         n -= 1
