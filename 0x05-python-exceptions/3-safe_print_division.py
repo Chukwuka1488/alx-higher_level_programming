880d1b076d1905393e1f67f5befeb828fc01fc91
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-08-28 08:34:50 -0700

    refactoring

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -10,5 +10,4 @@ def safe_print_division(a, b):
             print("Inside result: {:.1f}".format(result))
         else:
             print("Inside result: None")
-        return result
-
+        return (result)
