5ebe8443e2a3557b4bd7cb0eb56adcacebf04a5d
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-08-28 10:17:48 -0700

    PyFloatObject

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
 void print_python_list(PyObject *p)
 {
     long int size, i;
