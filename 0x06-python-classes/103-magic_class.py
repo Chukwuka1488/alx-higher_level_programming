731e5e146b06ac30026535e6c55bfb1d4194d393
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-08-29 13:17:04 -0700

    python classes and objects

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,27 +1,39 @@
+#!/usr/bin/python3
+"""Defines a class MagicClass"""
+
 import math
-import dis
 
 class MagicClass:
+    """
+    Class that defines properties of MagicClass.
+
+    Attributes:
+        radius: radius.
+    """
     def __init__(self, radius=0):
+        """Creates new instances of MagicClass.
+
+        Args:
+            radius: radius.
+
+        Raises:
+            TypeError: radius must be a number .
+        """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
+        """Returns area
+
+        Returns: area.
+        """
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """Returns circumference
+
+        Returns: circumference.
+        """
         return 2 * math.pi * self.__radius
-
-# Disassemble the __init__ method
-print("Disassembly of __init__:")
-dis.dis(MagicClass.__init__)
-
-# Disassemble the area method
-print("\nDisassembly of area:")
-dis.dis(MagicClass.area)
-
-# Disassemble the circumference method
-print("\nDisassembly of circumference:")
-dis.dis(MagicClass.circumference)
