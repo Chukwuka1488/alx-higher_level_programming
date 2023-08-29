af004d4c284be925579791092ecaec7feb69ece7
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-08-29 13:00:57 -0700

    python classes and objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
 class Square:
-    """Square class with private instance attribute size"""
+    """Square class with private instance attribute size
+
+    Attributes:
+        size: size of a square (1 side).
+    """
 
     def __init__(self, size):
         """Initialize a new Square object
