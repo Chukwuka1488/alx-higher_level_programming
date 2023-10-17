4495ab46afdc5446145931d857f07cb220563701
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-10-17 19:59:52 +0200

    temperatures 1

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- a script that displays the top 3 of cities temperature
 SELECT city, AVG(value) as avg_temp
 FROM temperatures
-WHERE MONTH(month) IN (7, 8)
+WHERE month = 7 OR month = 8
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
