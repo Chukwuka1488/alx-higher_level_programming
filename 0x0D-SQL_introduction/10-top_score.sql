290ba5de86431f1656e1934f19539fcdbd49c92e
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-10-17 18:04:43 +0200

    list by best

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- script that lists all records of the table second_table of the database by top score first
-SELECT * FROM `second_table`
-ORDER BY score;
+SELECT `score`, `name` FROM `second_table`
+ORDER BY score DESC;
