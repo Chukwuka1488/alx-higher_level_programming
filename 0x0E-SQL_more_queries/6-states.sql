6e7465713a9fc5b942baee531e05cb0c515d209f
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-10-18 05:58:46 -0700

    states table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- create a database hbtn_0d_usa and create a table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256),
+	UNIQUE KEY (id)
+)
