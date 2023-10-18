900a241b84ed1bdcc1cbd6fa0b29d0a3c4312b0e
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-10-18 06:32:14 -0700

    cities by states

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,8 @@
 -- script that lists all cities contained in the database
+
+-- Lists all cities and show the name of their state
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states on cities.id = states.id
+INNER JOIN states
+ON cities.id = states.id
 ORDER BY cities.id ASC;
