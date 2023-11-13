93f25ef5bc9e43d991bdda07407bc0bdefb61f68
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-11-13 10:51:51 -0800

    file that modifies the value of myVar to 333

---
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// File: 100-let_me_const.js
+
+global.myVar = 333;
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 // Script that finds the second biggest integer in the list of arguments
 
-function findSecondBiggest(args) {
+function findSecondBiggest (args) {
   if (args.length <= 1) {
     return 0;
   }
 
-  let numbers = args.map(Number); // Convert all arguments to numbers
-  let uniqueNumbers = Array.from(new Set(numbers)); // Remove duplicates
+  const numbers = args.map(Number); // Convert all arguments to numbers
+  const uniqueNumbers = Array.from(new Set(numbers)); // Remove duplicates
   uniqueNumbers.sort((a, b) => b - a); // Sort in descending order
 
   return uniqueNumbers[1]; // Return the second element
