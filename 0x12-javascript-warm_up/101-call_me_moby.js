abc35c185251bac8c1bf202efb881b53510e4977
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-11-13 10:55:41 -0800

    function that executes x times a function.

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// File: 101-call_me_moby.js
+
+function callMeMoby (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+
+module.exports.callMeMoby = callMeMoby;
