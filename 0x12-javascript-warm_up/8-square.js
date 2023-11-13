be19a48f8546be7ea3a93d3d8a8283d40bcfb4ee
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-11-13 09:55:56 -0800

    script that prints x times “C is fun”

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+// Script that prints a square of a given size without using repeat
+
+const args = process.argv.slice(2);
+const size = parseInt(args[0]);
+
+if (isNaN(size) || size < 1) {
+    console.log('Missing size');
+} else {
+    for (let i = 0; i < size; i++) {
+        let line = '';
+        for (let j = 0; j < size; j++) {
+            line += 'X';
+        }
+        console.log(line);
+    }
+}
