7e94da83bf2ac1f5294618b6174d8a13a3aa8d88
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-11-13 09:46:42 -0800

    first argument can be converted to an integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// first argument can be converted to an integer
+
+const firstArg = process.argv[2];
+const number = parseInt(firstArg);
+
+if (isNaN(number)) {
+  console.log("Not a number");
+} else {
+  console.log("My number: " + number);
+}
