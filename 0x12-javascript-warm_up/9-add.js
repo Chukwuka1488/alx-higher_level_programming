71ddcd0460096fea517264c4ef06cc853e832dc5
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-11-13 10:05:16 -0800

    script that prints the addition of 2 integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+// Script that prints the addition of two integers
+
+function add(a, b) {
+  return a + b;
+}
+
+const args = process.argv.slice(2);
+const firstNum = parseInt(args[0]);
+const secondNum = parseInt(args[1]);
+
+const result = add(firstNum, secondNum);
+console.log(result);
