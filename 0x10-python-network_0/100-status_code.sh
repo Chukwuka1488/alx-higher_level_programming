fe925ba2c11c5ec2b644c9dcdeb4fd99bd64f400
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-12-04 14:45:58 +0100

    body size

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # This script takes a URL as an argument, sends a request to that URL, and displays only the status code of the response.
-
 url=$1
 curl -s -o /dev/null -w "%{http_code}" "$url"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # This script takes a URL as an argument, sends a request to that URL, and displays all HTTP methods the server will accept.
-
 url=$1
 curl -sI "$url" | grep Allow | cut -d' ' -f2-
