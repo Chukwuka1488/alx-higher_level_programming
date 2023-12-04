88aaf2e89c94883c34164e4a17beee14aa3a1419
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-12-04 14:42:18 +0100

    body size

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # This script takes a URL as an argument, sends a GET request to that URL, and displays the body of the response if the status code is 200.
-
-url=$1
-curl -sL -w "%{http_code}" "$url" -o /dev/null | grep -q "200" && curl -s "$url"
+curl -sL -w "%{http_code}" "$1" -o /dev/null | grep -q "200" && curl -s "$1"
